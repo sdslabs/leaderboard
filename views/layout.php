7118c377c8a8a18a3af9c1cf91aa4832667bdcf6
Abhishek Das <das.abhshk@gmail.com>
2012-12-27 23:05:02 +0530

    Starting CSS

--- views/layout.php
+++ views/layout.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<title>Leaderboard | SDSLabs</title>
-		<link href="public/style.css" type="text/css" rel="stylesheet">
+		<link href="public/css/style.css" type="text/css" rel="stylesheet">
 	</head>
 	<body>
 		<?=$content?>
