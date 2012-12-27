6d4ed42d2abddf44e1136f95645385999852c0cf
Abhishek Das <das.abhshk@gmail.com>
2012-12-28 00:09:55 +0530

    Add favicon

--- views/layout.php
+++ views/layout.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 		<title>Leaderboard | SDSLabs</title>
+		<link rel="icon" type="image/x-icon" href="public/img/favicon.ico" />
 		<link href="public/css/bootstrap.min.css" type="text/css" rel="stylesheet">
 		<link href="public/css/style.css" type="text/css" rel="stylesheet">
 	</head>
