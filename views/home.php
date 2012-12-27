452008e23e9bc976c1f050a213d2415b41f5b469
Abhishek Das <das.abhshk@gmail.com>
2012-12-27 23:23:40 +0530

    Add Bootstrap Cosmo: Black topbar & table

--- views/home.php
+++ views/home.php
@@ -11,27 +11,45 @@ $services=array_unique($services);
 foreach($scores as $score)
 	$scores_formatted[$score->userid][$score->service]=$score->score;
 ?>
-<table>
-	<thead>
-		<tr>
-			<th>
-				UserId
-			</th>
-			<?foreach($services as $s):?>
-			<th><?=$s?></th>		
-			<?endforeach;?>
-		</tr>
-	</thead>
-	<tbody>
-		<?foreach($scores_formatted as $person=>$score):?>
-		<tr>
-			<th><?=$person?></th>
-			<?foreach($services as $s):?>
-			<td><?=@$score[$s]?$score[$s]:"N/A"?></td>
-			<?endforeach;?>
-		</tr>
-		<?endforeach;?>
-	</tbody>
 
+<div class="navbar navbar-fixed-top">
+	<div class="navbar-inner">
+		<div class="container">
+			<a class="brand" href="/">Leaderboard</a>
+			<div class="nav-collapse" id="main-menu">
+				<ul class="nav pull-right" id="main-menu-left">
+					<li><a href="/login/github">Login using Github</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
 
-</table>
+<section id="scores">
+	<div class="container">
+		<div class="row">
+			<table class="table table-bordered table-striped">
+				<thead>
+					<tr>
+						<th>
+							UserId
+						</th>
+						<?foreach($services as $s):?>
+						<th><?=$s?></th>		
+						<?endforeach;?>
+					</tr>
+				</thead>
+				<tbody>
+					<?foreach($scores_formatted as $person=>$score):?>
+					<tr>
+						<th><?=$person?></th>
+						<?foreach($services as $s):?>
+						<td><?=@$score[$s]?$score[$s]:"N/A"?></td>
+						<?endforeach;?>
+					</tr>
+					<?endforeach;?>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</section>
--- views/layout.php
+++ views/layout.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 		<title>Leaderboard | SDSLabs</title>
+		<link href="public/css/bootstrap.min.css" type="text/css" rel="stylesheet">
 		<link href="public/css/style.css" type="text/css" rel="stylesheet">
 	</head>
 	<body>
