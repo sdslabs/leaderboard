da51de1ad4e979c25d7b25e7eac9afb5a972da0f
Abhay Rana <capt.n3m0@gmail.com>
2012-12-27 15:39:39 +0530

    We have a UI (table) for the homepage

--- views/home.php
+++ views/home.php
@@ -1,5 +1,37 @@
-All scores:
-
 <?php
-print_r($scores);
+$scores_formatted=array();	//Keep final formatted scores here
+
+//generate a list of unique service names
+$services=array_map(function($row){
+	return $row->service;
+},$scores);
+$services=array_unique($services);
+
+//create a 2d table for user/scores
+foreach($scores as $score)
+	$scores_formatted[$score->userid][$score->service]=$score->score;
 ?>
+<table>
+	<thead>
+		<tr>
+			<th>
+				UserId
+			</th>
+			<?foreach($services as $s):?>
+			<th><?=$s?></th>		
+			<?endforeach;?>
+		</tr>
+	</thead>
+	<tbody>
+		<?foreach($scores_formatted as $person=>$score):?>
+		<tr>
+			<th><?=$person?></th>
+			<?foreach($services as $s):?>
+			<td><?=@$score[$s]?$score[$s]:"N/A"?></td>
+			<?endforeach;?>
+		</tr>
+		<?endforeach;?>
+	</tbody>
+
+
+</table>
--- views/layout.php
+++ views/layout.php
@@ -1,2 +1,10 @@
-HELLO
-<?=$content?>
+<!doctype html>
+<html>
+	<head>
+		<title>Leaderboard | SDSLabs</title>
+		<link href="public/style.css" type="text/css" rel="stylesheet">
+	</head>
+	<body>
+		<?=$content?>
+	</body>
+</html>
