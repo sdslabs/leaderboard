f8e9ede849f88683f7249b70a49f69672da0a5a8
Abhay Rana <capt.n3m0@gmail.com>
2012-12-30 11:20:26 +0530

    Added a footer

--- views/layout.php
+++ views/layout.php
@@ -7,24 +7,32 @@
 		<link href="/public/css/style.css" type="text/css" rel="stylesheet">
 	</head>
 	<body>
-		<div class="navbar navbar-fixed-top">
-			<div class="navbar-inner">
-				<div class="container">
-					<a class="brand" href="/">Leaderboard</a>
-					<div class="nav-collapse" id="main-menu">
-						<ul class="nav pull-right" id="main-menu-left">
-							<?if (isset($_SESSION['userid']) && $_SESSION['userid']!==false):?>
-							<li id="login-info"><a>Logged in as <?=$_SESSION['userid'];?></a></li>
-							<li id="manage"><a href="/accounts">Manage Accounts</a></li>
-							<li><a href="/logout">Logout</a></li>
-							<?else:?>
-							<li id="login-button"><a href="/login/github"><img id="gh-icon" src="/public/img/gh.png">Login</a></li>
-							<?endif;?>
-						</ul>
+		<div id="wrap">
+			<div class="navbar navbar-fixed-top">
+				<div class="navbar-inner">
+					<div class="container">
+						<a class="brand" href="/">Leaderboard</a>
+						<div class="nav-collapse" id="main-menu">
+							<ul class="nav pull-right" id="main-menu-left">
+								<?if (isset($_SESSION['userid']) && $_SESSION['userid']!==false):?>
+								<li id="login-info"><a>Logged in as <?=$_SESSION['userid'];?></a></li>
+								<li id="manage"><a href="/accounts">Manage Accounts</a></li>
+								<li><a href="/logout">Logout</a></li>
+								<?else:?>
+								<li id="login-button"><a href="/login/github"><img id="gh-icon" src="/public/img/gh.png">Login</a></li>
+								<?endif;?>
+							</ul>
+						</div>
 					</div>
 				</div>
 			</div>
+			<?=$content?>
 		</div>
-		<?=$content?>
+		<div id="footer">
+	      <div class="container">
+	        <p>Designed and build by <a href="http://team.sdslabs.co">Team SDSLabs</a>.</p>
+	        <p>Code licenced under the MIT Licence and available on <a href="https://github.com/sdslabs/leaderboard/">GitHub</a>.
+	      </div>
+	    </div>
 	</body>
 </html>
