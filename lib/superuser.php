86dabd4c74b5a6aacc906beece6ec784d620161c
Abhishek Das <das.abhshk@gmail.com>
2013-02-04 01:10:57 +0530

    Add superuser, since most of us gained rep on it during the Windows 8 challenge.

---
+++ lib/superuser.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Required Configuration
+ * STACKEXCHANGE_KEY : Key of your SE App
+ */
+class Superuser{
+  const name="superuser";
+  public static function login()
+    {
+    set('service',self::name);
+    set('prepend','superuser.com/users/');
+    set('append','/user-name');
+    return render('ask_username.php');
+    }
+    public static function update($userid)
+    {
+      global $HTTP_CONFIG;
+      $id=Token::get(self::name,$userid);
+    $request=new HTTP_Request2("https://api.stackexchange.com/2.1/users/".$id."?site=superuser&key=".STACKEXCHANGE_KEY);
+    $request->setConfig($HTTP_CONFIG);
+    $response = $request->send()->getBody();
+    $reputation=json_decode($response)->items[0]->reputation;
+    Score::update(self::name,$userid,$reputation);//Update in database
+    }
+  public static function callback()
+  {
+    //Get the username
+    $username=$_GET['username'];
+    //Save it inside as an access_token
+    Token::add(self::name,$_SESSION['userid'],$username);
+    redirect_to('/');
+  }
+}
